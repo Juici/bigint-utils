7857214da4f50ac6769a0717de053e49e04cadb2
James Whaley <juicy66173@gmail.com>
2022-06-04 11:33:47 +0100

    Clean up native module code

--- src/node/native/module.c
+++ src/node/native/module.c
@@ -114,11 +114,13 @@ typedef struct bigint {
     allocation buf;
 } bigint;
 
-static bool get_bigint_words(napi_env env, napi_callback_info info, bigint *b) {
+static bigint get_bigint_words(napi_env env, napi_callback_info info) {
     napi_status status;
 
     napi_value argv[1];
     size_t argc = 1;
+
+    bigint b;
 
     status = napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
     if (status != napi_ok) {
@@ -126,40 +128,43 @@ static bool get_bigint_words(napi_env env, napi_callback_info info, bigint *b) {
     }
     if (argc != 1) {
         napi_throw_type_error(env, "ERR_MISSING_ARGS", "Wrong number of arguments");
-        return false;
+        goto fail;
     }
 
-    status = napi_get_value_bigint_words(env, argv[0], NULL, &b->word_count, NULL);
+    status = napi_get_value_bigint_words(env, argv[0], NULL, &b.word_count, NULL);
     if (status != napi_ok) {
         napi_throw_type_error(env, "ERR_INVALID_ARG_TYPE", "Expected argument to be a bigint");
-        return false;
+        goto fail;
     }
 
-    b->buf = alloc(env, b->word_count);
+    b.buf = alloc(env, b.word_count);
 
     int sign_bit;
 
-    status = napi_get_value_bigint_words(env, argv[0], &sign_bit, &b->word_count, b->buf.data);
+    status = napi_get_value_bigint_words(env, argv[0], &sign_bit, &b.word_count, b.buf.data);
     if (status != napi_ok) {
         napi_fatal_error(NULL, 0, "Failed to get bigint words", NAPI_AUTO_LENGTH);
     }
 
-    return true;
+    return b;
+
+fail:
+    b.buf.data = NULL;
+    return b;
 }
 
 napi_value to_bytes_le(napi_env env, napi_callback_info info) {
     napi_status status;
 
-    bigint b;
-
-    if (!get_bigint_words(env, info, &b)) {
+    bigint b = get_bigint_words(env, info);
+    if (b.buf.data == NULL) {
         return NULL;
     }
 
-    size_t len = b.word_count * sizeof(uint64_t);
+    size_t len = 0;
     if (b.word_count > 0) {
         uint64_t tail = b.buf.data[b.word_count - 1];
-        len -= clzb(tail);
+        len = (b.word_count * sizeof(uint64_t)) - clzb(tail);
     }
 
     napi_value result;
@@ -176,8 +181,8 @@ napi_value to_bytes_le(napi_env env, napi_callback_info info) {
 napi_value to_bytes_be(napi_env env, napi_callback_info info) {
     napi_status status;
 
-    bigint b;
-    if (!get_bigint_words(env, info, &b)) {
+    bigint b = get_bigint_words(env, info);
+    if (b.buf.data == NULL) {
         return NULL;
     }
 
@@ -205,11 +210,13 @@ typedef struct uint8_array {
     size_t len;
 } uint8_array;
 
-static bool get_uint8_array(napi_env env, napi_callback_info info, uint8_array *array) {
+static uint8_array get_uint8_array(napi_env env, napi_callback_info info) {
     napi_status status;
 
     napi_value argv[1];
     size_t argc = 1;
+
+    uint8_array a;
 
     status = napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
     if (status != napi_ok) {
@@ -217,19 +224,22 @@ static bool get_uint8_array(napi_env env, napi_callback_info info, uint8_array *
     }
     if (argc != 1) {
         napi_throw_type_error(env, "ERR_MISSING_ARGS", "Wrong number of arguments");
-        return false;
+        goto fail;
     }
 
     napi_typedarray_type type;
 
-    status = napi_get_typedarray_info(env, argv[0], &type, &array->len, (void **) &array->data,
-                                      NULL, NULL);
+    status = napi_get_typedarray_info(env, argv[0], &type, &a.len, (void **) &a.data, NULL, NULL);
     if (status != napi_ok || type != napi_uint8_array) {
         napi_throw_type_error(env, "ERR_INVALID_ARG_TYPE", "Expected argument to be a Uint8Array");
-        return false;
+        goto fail;
     }
 
-    return true;
+    return a;
+
+fail:
+    a.data = NULL;
+    return a;
 }
 
 // TODO: Use alloc pool for from_bytes as well?
@@ -240,8 +250,8 @@ napi_value from_bytes_le(napi_env env, napi_callback_info info) {
 
     uint64_t stack_buf[MAX_STACK_WORDS];
 
-    uint8_array a;
-    if (!get_uint8_array(env, info, &a)) {
+    uint8_array a = get_uint8_array(env, info);
+    if (a.data == NULL) {
         return NULL;
     }
 
@@ -259,22 +269,20 @@ napi_value from_bytes_le(napi_env env, napi_callback_info info) {
     }
 
     word_count += (remainder > 0);
+    remainder = 8 - remainder;
 
     bool fits_in_stack = word_count <= MAX_STACK_WORDS;
 
     uint64_t *buf = stack_buf;
     if (!fits_in_stack) {
-        buf = malloc(a.len + remainder);
+        buf = malloc(word_count * sizeof(uint64_t));
         if (buf == NULL) {
             napi_throw_error(env, "ERR_MEMORY_ALLOCATION_FAILED", "Failed to allocate memory");
             return NULL;
         }
     }
 
-    if (remainder > 0) {
-        // Zero the trailing bytes.
-        memset(buf + a.len, 0, remainder);
-    }
+    memset(&((uint8_t *) buf)[a.len], 0, remainder);
     memcpy(buf, a.data, a.len);
 
     status = napi_create_bigint_words(env, 0, word_count, (uint64_t *) a.data, &result);
@@ -298,8 +306,8 @@ napi_value from_bytes_be(napi_env env, napi_callback_info info) {
 
     uint64_t stack_buf[MAX_STACK_WORDS];
 
-    uint8_array a;
-    if (!get_uint8_array(env, info, &a)) {
+    uint8_array a = get_uint8_array(env, info);
+    if (a.data == NULL) {
         return NULL;
     }
 
@@ -364,6 +372,8 @@ NAPI_MODULE_INIT() {
 
     EXPORT_FUNCTION(env, from_bytes_le, "fromBytesLE", fail);
     EXPORT_FUNCTION(env, from_bytes_be, "fromBytesBE", fail);
+
+#undef EXPORT_FUNCTION
 
     return exports;
 
